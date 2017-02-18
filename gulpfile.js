701ef9d54cb81da23ba781f618ad4587dc54267c
David Aerne <meodai@gmail.com>
2017-02-18 12:41:24 +0100

    more tasky tasky

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 'use strict';
 
 const gulp = require('gulp'),
+      // css
       sass = require('gulp-sass'),
       postcss = require('gulp-postcss'),
       autoprefixer = require('autoprefixer'),
       cssnano = require('cssnano'),
+
+      // images
+      imagemin = require('gulp-imagemin'),
+
+      // system
       del = require('del');
 
-gulp.task('clean', function () {
-  return del(paths.build);
+gulp.task('clean', () => {
+  return del('./build/');
 });
 
-gulp.task('css', function () {
+gulp.task('css', ['clean'], () => {
   const plugins = [
     autoprefixer({browsers: ['last 2 versions']}),
     cssnano()
@@ -22,6 +28,14 @@ gulp.task('css', function () {
     .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('css:watch', function () {
+gulp.task('css:watch', () => {
   gulp.watch('./src/**/*.scss', ['css']);
 });
+
+gulp.task('images', ['clean'], () => {
+  return gulp.src('./data/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images/'))
+})
+
+gulp.task('default', ['clean', 'css', 'images']);
